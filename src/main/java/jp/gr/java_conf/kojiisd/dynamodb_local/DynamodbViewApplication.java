402b50f0bdeb68c1001e9d5d0281df96415b4d65
kishida58 <>
2022-02-02 08:11:30 +0900

    CodeGuruを不用意に利用していたので除外。

--- src/main/java/jp/gr/java_conf/kojiisd/dynamodb_local/DynamodbViewApplication.java
+++ src/main/java/jp/gr/java_conf/kojiisd/dynamodb_local/DynamodbViewApplication.java
@@ -13,7 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.util.StringUtils;
-import software.amazon.codeguruprofilerjavaagent.Profiler;
 
 /**
  * @author kojiisd
@@ -25,9 +24,6 @@ public class DynamodbViewApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(DynamodbViewApplication.class, args);
-        new Profiler.Builder()
-                .profilingGroupName("sample-profiling-group")
-                .build().start();
     }
 
     @Value("${amazon.dynamodb.endpoint}")
